2bc71c858094b47e289744e7eccdd73166f807b4
Pedro <pfcaraujo@uolinc.com>
2019-01-11 17:29:28 -0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Pedro Felipe Coloma de Araujo on 11/01/19.
+//  Copyright © 2019 Pedro Felipe Coloma de Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
